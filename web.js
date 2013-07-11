037f9d3ae1880377ede5e89a2e3bbf4e1e54aa68
Edgardo Moreira <moreirab@hotmail.com>
2013-07-11 04:23:46 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-var fs = require('fs')
+// var fs = require('fs')
 
 var app = express.createServer(express.logger());
 
@@ -8,6 +8,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
