a6995056bf255555d9714370af6c2ac38bb5c8f1
Edgardo Moreira <moreirab@hotmail.com>
2013-07-11 03:46:50 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,13 +4,12 @@ var fs = require('fs')
 
 var app = express.createServer(express.logger());
 
-var buf =  fs.readFile('index.html');
+var buf =  fs.readFileSync("index.html");
 
 var cont = buf.toString('utf8');
 
 app.get('/', function(request, response) {
   response.send(cont);
-});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
