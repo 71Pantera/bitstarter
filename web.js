52a8be1cd53205e04814734551b4c3925421a9d8
Edgardo Moreira <moreirab@hotmail.com>
2013-07-11 04:05:38 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 
 var buf =  fs.readFileSync("index.html");
 
-var cont = buf.toString('utf8');
+//var cont = buf.toString('utf8');
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
