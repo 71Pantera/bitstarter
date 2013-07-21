300d981c7baee0c3068a0739f8c26f591393c704
71Pantera <moreirab@hotmail.com>
2013-07-21 23:33:55 +0000

    web.js updated

--- web.js
+++ web.js
@@ -4,15 +4,15 @@ var fs = require('fs')
 
 var app = express.createServer(express.logger());
 
-var buf =  fs.readFileSync("index.html");
+var buf =  fs.readFileSync("index.html").toString();
 
 var cont = buf.toString('utf8');
 
 app.get('/', function(request, response) {
-  response.send(cont);
+  response.send(buf);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
